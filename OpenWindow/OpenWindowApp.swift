fc479516eac737fcff49f16efd7e472047a8599f
Tim Mitra <tim@it-guy.com>
2024-08-28 00:37:50 -0400

    Initial Commit

---
+++ OpenWindow/OpenWindowApp.swift
@@ -0,0 +1,21 @@
+//
+// ---------------------------- //
+// Original Project: OpenWindow
+// Created on 2024-08-28 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct OpenWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
