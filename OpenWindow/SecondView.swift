3f75f0997a8d6465040df6dc66ed38cfa29f9690
Tim Mitra <tim@it-guy.com>
2024-08-28 00:52:26 -0400

    add second windowGroup with id

--- OpenWindow/SecondView.swift
+++ OpenWindow/SecondView.swift
@@ -11,14 +11,12 @@
 
 import SwiftUI
 
-@main
-struct OpenWindowApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        WindowGroup(id: "SecondView") {
-            SecondView()
-        }
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+#Preview {
+    SecondView()
 }
